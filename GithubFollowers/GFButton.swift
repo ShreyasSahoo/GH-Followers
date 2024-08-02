30b5bc2574f748732275f86f296225d074bdb20d
Shreyas Sahoo <shreyassahoo26@gmail.com>
2024-08-02 10:13:34 +0530

    Initial Commit

---
+++ GithubFollowers/GFButton.swift
@@ -0,0 +1,20 @@
+//
+//  GFButton.swift
+//  GithubFollowers
+//
+//  Created by Shreyas Sahoo on 01/08/24.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ GithubFollowers/GFTextField.swift
@@ -0,0 +1,20 @@
+//
+//  GFTextField.swift
+//  GithubFollowers
+//
+//  Created by Shreyas Sahoo on 01/08/24.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
